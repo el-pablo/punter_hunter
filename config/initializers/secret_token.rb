c5e81c600c8c2d6dfcc3c55fa037fe5f9a47a190
Paul Thomas-FMThomap12T <thomap12@mc-s056123.lan>
2011-08-25 21:37:17 +0100

    Initial commit - everything set up

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PunterHunter::Application.config.secret_token = '0b78857c6fb29c997ef977837c34fd1d21255e660761b345fa866aca07ecae6b426a0b2b887ba7723ab1275cedc172a912e71fc4054591867518285d8a6405f0'
